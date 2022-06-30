842e1b766dbe79ea08a40e51f295fdd88a151fe6
Ann <ania.somko@gmail.com>
2022-06-30 21:47:34 +0300

    change index.js and imagesAPI.js files

--- src/imagesAPI.js
+++ src/imagesAPI.js
@@ -1,27 +1,36 @@
-export default class imagesAPI {
+const API_KEY = '28329957-abfecd61523ca859322fdae14';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default class ImagesApiService {
   constructor() {
-    this.formInputValue = '';
+    this.inputQuery = '';
+    this.page = 1;
   }
 
   fetchImages() {
-    console.log(this);
-    let API_KEY = '28329957-abfecd61523ca859322fdae14';
-    let url =
-      'https://pixabay.com/api/?key=' +
-      API_KEY +
-      '&q=' +
-      encodeURIComponent(this.formInputValue) +
-      '&image_type=photo&orientation=horizontal&safesearch=true';
-    fetch(url)
-      .then(r => r.json())
-      .then(console.log);
+    const url = `${BASE_URL}?key=${API_KEY}&q=${this.inputQuery}&image_type=photo
+    &orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.incrementPage();
+        return data.hits;
+      });
   }
 
-  get value() {
-    return this.formInputValue;
+  incrementPage() {
+    this.page += 1;
   }
 
-  set value(newValue) {
-    this.formInputValue = newValue;
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.inputQuery;
+  }
+
+  set query(newQuery) {
+    this.inputQuery = newQuery;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
-import imagesAPI from './imagesAPI';
+import ImagesApiService from './imagesAPI';
 
-const refs = {
-  searchForm: document.querySelector('#search-form'),
-  imagesContainer: document.querySelector('.gallery'),
-  loadMore: document.querySelector('.load-more'),
-};
+let searchForm = document.querySelector('#search-form');
+let imagesContainer = document.querySelector('.gallery');
+let loadMore = document.querySelector('.load-more');
 
-const images = new imagesAPI();
+searchForm.addEventListener('submit', onSearch);
+loadMore.addEventListener('click', onLoadMore);
 
-refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMore.addEventListener('click', onLoadMore);
+const imagesApiService = new ImagesApiService();
 
 function onSearch(e) {
   e.preventDefault();
-  formInputValue.value = e.currentTarget.elements.searchQuery.value;
-  images.fetchImages();
+
+  imagesApiService.query = e.currentTarget.elements.searchQuery.value;
+  imagesApiService.resetPage();
+  imagesApiService.fetchImages().then(hits => {
+    clearImagesContainer();
+    renderImagesMarkup(hits);
+  });
 }
 
 function onLoadMore() {
-  images.fetchImages();
+  imagesApiService.fetchImages().then(renderImagesMarkup);
+}
+
+function renderImagesMarkup(hits) {
+  const markup = hits
+    .map(image => {
+      `<div class="photo-card">
+    <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes ${image.likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views ${image.views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments ${image.comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads ${image.downloads}</b>
+      </p>
+    </div>
+  </div>`;
+    })
+    .join('');
+  imagesContainer.innerHTML = markup;
+}
+
+function clearImagesContainer() {
+  imagesContainer.innerHTML = '';
 }
